089748942c9203db4ebece8f1e557399bc5e18f9
HoaiPhong1411 <96569661+HoaiPhong1411@users.noreply.github.com>
2022-04-18 15:46:02 +0700

    update editProduct

--- src/Container/Client/ClientEditProduct/ClientEditProduct.jsx
+++ src/Container/Client/ClientEditProduct/ClientEditProduct.jsx
@@ -19,7 +19,7 @@ const ClientEditProduct = () => {
   // showDropDown
   const [dropDown, setDropDown] = useState(false);
   // show Product Choose
-  const [product, setProduct] = useState([]);
+  const [product, setProduct] = useState(null);
   const [load, setLoad] = useState(false);
   // get File Image
   const [file, setFile] = useState();
@@ -31,9 +31,11 @@ const ClientEditProduct = () => {
   const [display, setDisplay] = useState();
   const [position, setPosition] = useState();
 
-  const getEdit = useSelector((state) => state.product.pro);
-  console.log(getEdit);
-
+  const [editPro, setEditPro] = useState([]);
+  const getEdit = useSelector((state) => state.product.product.product);
+  useEffect(() => {
+    setEditPro(getEdit);
+  }, []);
   //   Lấy product từ database
   useEffect(() => {
     const fecth = async () => {
@@ -119,19 +121,21 @@ const ClientEditProduct = () => {
     e.preventDefault();
     const id = document.querySelector(".id").innerHTML;
     const dataUpdate = new FormData();
-    dataUpdate.append("title", title ?? product[0].title);
-    dataUpdate.append("description", des ?? product[0].description);
-    dataUpdate.append("price", price ?? product[0].price);
-    dataUpdate.append("photo", file ?? product[0].photo);
-    if (display) {
-      dataUpdate.append("display", +1);
+    if (product) {
+      dataUpdate.append("title", title ?? product[0].title);
+      dataUpdate.append("description", des ?? product[0].description);
+      dataUpdate.append("price", price ?? product[0].price);
+      dataUpdate.append("photo", file ?? product[0].photo);
+      dataUpdate.append("display", display ? +1 : +0);
+      dataUpdate.append("position", position ? +1 : +0);
     } else {
-      dataUpdate.append("display", +0);
-    }
-    if (position) {
-      dataUpdate.append("position", +1);
-    } else {
-      dataUpdate.append("position", +0);
+      // update
+      dataUpdate.append("title", title ?? editPro[0].title);
+      dataUpdate.append("description", des ?? editPro[0].description);
+      dataUpdate.append("price", price ?? editPro[0].price);
+      dataUpdate.append("photo", file ?? editPro[0].photo);
+      dataUpdate.append("display", display ? +1 : +0);
+      dataUpdate.append("position", position ? +1 : +0);
     }
 
     updateProduct(id, dataUpdate);
@@ -140,12 +144,7 @@ const ClientEditProduct = () => {
   };
   const updateProduct = async (id, data) => {
     try {
-      const response = await axios.post(
-        `http://localhost:8000/api/product/update/${id}`,
-        data
-      );
-
-      console.log("success", response);
+      await axios.post(`http://localhost:8000/api/product/update/${id}`, data);
     } catch (error) {
       console.log(error);
     }
@@ -218,117 +217,233 @@ const ClientEditProduct = () => {
       </div>
       <div className="flex flex-row gap-5 w-full bg-primary py-5 px-10 rounded-xl mt-7">
         <form className="w-full" action="" onSubmit={(e) => handleSubmit(e)}>
-          {product?.map((item) => (
-            <table className="w-full text-secondary flex flex-col gap-5">
-              <div className="w-full flex flex-col gap-5 justify-between items-center text-[#fff]">
-                <div
-                  className="flex flex-col w-full justify-between items-center
-                 gap-5"
-                >
-                  <img
-                    src={img ?? urlImg + item.photo}
-                    alt=""
-                    className="w-[60%] bg-cover border-2 border-secondary"
-                  />
-                  <input
-                    type="file"
-                    name="photo"
-                    id="photo"
-                    accept="image/*"
-                    className="hidden"
-                    onChange={(e) => handleChangeImage(e)}
-                  />
-                  <ButtonUpload htmlFor="photo" />
-                </div>
-              </div>
-              <div className="hidden">
-                <label className="id">{item.id}</label>
-              </div>
-              <div className="w-full flex flex-col justify-between gap-2 items-start text-[#fff]">
-                <label htmlFor="title">Title</label>
-                <input
-                  type="text"
-                  value={title ?? item.title}
-                  className="w-full border-[1px] border-secondary bg-primary focus:border-[#e0ed2e] font-light"
-                  onChange={(e) => handleChangeTitle(e)}
-                />
-              </div>
-              <div className="w-full flex flex-col justify-between gap-2 items-start text-[#fff]">
-                <label htmlFor="description">Description</label>
-                <textarea
-                  rows="3"
-                  type="text"
-                  value={des ?? item.description}
-                  className="w-full border-[1px] border-secondary bg-primary focus:border-[#e0ed2e] font-light"
-                  onChange={(e) => handleChangeDes(e)}
-                />
-              </div>
-              <div className="w-full flex flex-col justify-between gap-2 items-start text-[#fff]">
-                <label htmlFor="price">Price</label>
-                <input
-                  type="text"
-                  value={price ?? item.price}
-                  className="w-full border-[1px] border-secondary bg-primary focus:border-[#e0ed2e] font-light"
-                  onChange={(e) => handleChangePrice(e)}
-                />
-              </div>
-              <div className="w-full flex flex-row justify-between items-center text-[#fff]">
-                <label htmlFor="display">Display</label>
-                <div className="w-[85%] ">
-                  <ButtonCheck
-                    htmlFor="display"
-                    idIcon="btn-display"
-                    style={
-                      item.display !== 1
-                        ? { color: "#fff" }
-                        : { color: "#04f604" }
-                    }
-                  />
-                </div>
-                <input
-                  type="checkbox"
-                  defaultChecked={item.display == 1 ? true : false}
-                  id="display"
-                  className="hidden"
-                  onChange={(e) => handleChangeDiplay(e)}
-                />
-              </div>
-              <div className="w-full flex flex-row justify-between items-center text-[#fff]">
-                <label htmlFor="position">Position</label>
-                <div className="w-[85%] ">
-                  <ButtonCheck
-                    htmlFor="position"
-                    idIcon="btn-position"
-                    style={
-                      item.position !== 1
-                        ? { color: "#fff" }
-                        : { color: "#04f604" }
-                    }
-                  />
-                </div>
-                <input
-                  type="checkbox"
-                  id="position"
-                  className="hidden"
-                  style={
-                    item.position !== 1
-                      ? { color: "#fff" }
-                      : { color: "#04f604" }
-                  }
-                  onChange={(e) => handleChangePosition(e)}
-                />
-              </div>
-              <div className="flex justify-center items-center">
-                <button
-                  type="submit"
-                  className="w-[14%] flex flex-row justify-center items-center p-2 rounded-lg cursor-pointer hover:bg-[#2a7ffe] text-[#fff] bg-secondary"
-                >
-                  <FiSave className="text-lg mr-4" />
-                  Save
-                </button>
-              </div>
-            </table>
-          ))}
+          {product
+            ? // pick from input
+              product?.map((item) => (
+                <table className="w-full text-secondary flex flex-col gap-5">
+                  <div className="w-full flex flex-col gap-5 justify-between items-center text-[#fff]">
+                    <div
+                      className="flex flex-col w-full justify-between items-center
+                   gap-5"
+                    >
+                      <img
+                        src={img ?? urlImg + item.photo}
+                        alt=""
+                        className="w-[60%] bg-cover border-2 border-secondary"
+                      />
+                      <input
+                        type="file"
+                        name="photo"
+                        id="photo"
+                        accept="image/*"
+                        className="hidden"
+                        onChange={(e) => handleChangeImage(e)}
+                      />
+                      <ButtonUpload htmlFor="photo" />
+                    </div>
+                  </div>
+                  <div className="hidden">
+                    <label className="id">{item.id}</label>
+                  </div>
+                  <div className="w-full flex flex-col justify-between gap-2 items-start text-[#fff]">
+                    <label htmlFor="title">Title</label>
+                    <input
+                      type="text"
+                      value={title ?? item.title}
+                      className="w-full border-[1px] border-secondary bg-primary focus:border-[#e0ed2e] font-light"
+                      onChange={(e) => handleChangeTitle(e)}
+                    />
+                  </div>
+                  <div className="w-full flex flex-col justify-between gap-2 items-start text-[#fff]">
+                    <label htmlFor="description">Description</label>
+                    <textarea
+                      rows="3"
+                      type="text"
+                      value={des ?? item.description}
+                      className="w-full border-[1px] border-secondary bg-primary focus:border-[#e0ed2e] font-light"
+                      onChange={(e) => handleChangeDes(e)}
+                    />
+                  </div>
+                  <div className="w-full flex flex-col justify-between gap-2 items-start text-[#fff]">
+                    <label htmlFor="price">Price</label>
+                    <input
+                      type="text"
+                      value={price ?? item.price}
+                      className="w-full border-[1px] border-secondary bg-primary focus:border-[#e0ed2e] font-light"
+                      onChange={(e) => handleChangePrice(e)}
+                    />
+                  </div>
+                  <div className="w-full flex flex-row justify-between items-center text-[#fff]">
+                    <label htmlFor="display">Display</label>
+                    <div className="w-[85%] ">
+                      <ButtonCheck
+                        htmlFor="display"
+                        idIcon="btn-display"
+                        style={
+                          item.display !== 1
+                            ? { color: "#fff" }
+                            : { color: "#04f604" }
+                        }
+                      />
+                    </div>
+                    <input
+                      type="checkbox"
+                      defaultChecked={item.display == 1 ? true : false}
+                      id="display"
+                      className="hidden"
+                      onChange={(e) => handleChangeDiplay(e)}
+                    />
+                  </div>
+                  <div className="w-full flex flex-row justify-between items-center text-[#fff]">
+                    <label htmlFor="position">Position</label>
+                    <div className="w-[85%] ">
+                      <ButtonCheck
+                        htmlFor="position"
+                        idIcon="btn-position"
+                        style={
+                          item.position !== 1
+                            ? { color: "#fff" }
+                            : { color: "#04f604" }
+                        }
+                      />
+                    </div>
+                    <input
+                      type="checkbox"
+                      id="position"
+                      className="hidden"
+                      style={
+                        item.position !== 1
+                          ? { color: "#fff" }
+                          : { color: "#04f604" }
+                      }
+                      onChange={(e) => handleChangePosition(e)}
+                    />
+                  </div>
+                  <div className="flex justify-center items-center">
+                    <button
+                      type="submit"
+                      className="w-[14%] flex flex-row justify-center items-center p-2 rounded-lg cursor-pointer hover:bg-[#2a7ffe] text-[#fff] bg-secondary"
+                    >
+                      <FiSave className="text-lg mr-4" />
+                      Save
+                    </button>
+                  </div>
+                </table>
+              ))
+            : // End pick from input
+              // pick from product
+              getEdit?.map((item) => (
+                <table className="w-full text-secondary flex flex-col gap-5">
+                  <div className="w-full flex flex-col gap-5 justify-between items-center text-[#fff]">
+                    <div
+                      className="flex flex-col w-full justify-between items-center
+                   gap-5"
+                    >
+                      <img
+                        src={img ?? urlImg + item.photo}
+                        alt=""
+                        className="w-[60%] bg-cover border-2 border-secondary"
+                      />
+                      <input
+                        type="file"
+                        name="photo"
+                        id="photo"
+                        accept="image/*"
+                        className="hidden"
+                        onChange={(e) => handleChangeImage(e)}
+                      />
+                      <ButtonUpload htmlFor="photo" />
+                    </div>
+                  </div>
+                  <div className="hidden">
+                    <label className="id">{item.id}</label>
+                  </div>
+                  <div className="w-full flex flex-col justify-between gap-2 items-start text-[#fff]">
+                    <label htmlFor="title">Title</label>
+                    <input
+                      type="text"
+                      value={title ?? item.title}
+                      className="w-full border-[1px] border-secondary bg-primary focus:border-[#e0ed2e] font-light"
+                      onChange={(e) => handleChangeTitle(e)}
+                    />
+                  </div>
+                  <div className="w-full flex flex-col justify-between gap-2 items-start text-[#fff]">
+                    <label htmlFor="description">Description</label>
+                    <textarea
+                      rows="3"
+                      type="text"
+                      value={des ?? item.description}
+                      className="w-full border-[1px] border-secondary bg-primary focus:border-[#e0ed2e] font-light"
+                      onChange={(e) => handleChangeDes(e)}
+                    />
+                  </div>
+                  <div className="w-full flex flex-col justify-between gap-2 items-start text-[#fff]">
+                    <label htmlFor="price">Price</label>
+                    <input
+                      type="text"
+                      value={price ?? item.price}
+                      className="w-full border-[1px] border-secondary bg-primary focus:border-[#e0ed2e] font-light"
+                      onChange={(e) => handleChangePrice(e)}
+                    />
+                  </div>
+                  <div className="w-full flex flex-row justify-between items-center text-[#fff]">
+                    <label htmlFor="display">Display</label>
+                    <div className="w-[85%] ">
+                      <ButtonCheck
+                        htmlFor="display"
+                        idIcon="btn-display"
+                        style={
+                          item.display !== 1
+                            ? { color: "#fff" }
+                            : { color: "#04f604" }
+                        }
+                      />
+                    </div>
+                    <input
+                      type="checkbox"
+                      defaultChecked={item.display == 1 ? true : false}
+                      id="display"
+                      className="hidden"
+                      onChange={(e) => handleChangeDiplay(e)}
+                    />
+                  </div>
+                  <div className="w-full flex flex-row justify-between items-center text-[#fff]">
+                    <label htmlFor="position">Position</label>
+                    <div className="w-[85%] ">
+                      <ButtonCheck
+                        htmlFor="position"
+                        idIcon="btn-position"
+                        style={
+                          item.position !== 1
+                            ? { color: "#fff" }
+                            : { color: "#04f604" }
+                        }
+                      />
+                    </div>
+                    <input
+                      type="checkbox"
+                      id="position"
+                      className="hidden"
+                      style={
+                        item.position !== 1
+                          ? { color: "#fff" }
+                          : { color: "#04f604" }
+                      }
+                      onChange={(e) => handleChangePosition(e)}
+                    />
+                  </div>
+                  <div className="flex justify-center items-center">
+                    <button
+                      type="submit"
+                      className="w-[14%] flex flex-row justify-center items-center p-2 rounded-lg cursor-pointer hover:bg-[#2a7ffe] text-[#fff] bg-secondary"
+                    >
+                      <FiSave className="text-lg mr-4" />
+                      Save
+                    </button>
+                  </div>
+                </table>
+              ))}
+          {/* End pick from product */}
         </form>
       </div>
     </>
